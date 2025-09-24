96faa105214b6ca23d4f5e6f90a0186faf7b4e3d
LuisR <luisrodriguezacosta@gmail.com>
2025-09-24 17:07:02 -0500

    Update advertising

--- src/features/hero/components/HeroSection.tsx
+++ src/features/hero/components/HeroSection.tsx
@@ -4,17 +4,17 @@ import Button from '@/shared/components/ui/Button'
 
 export default function HeroSection() {
   return (
-    <section className="relative w-full min-h-screen">
+    <section className="relative w-full min-h-screen isolate">
       <video
-        className="absolute inset-0 w-full h-full object-cover"
+        className="absolute inset-0 w-full h-full object-cover z-0"
         src="/video/video-b-parking.mp4"
         autoPlay
         muted
         loop
         playsInline
       />
-      <div className="absolute inset-0 bg-blue-600/30 mix-blend-multiply"></div>
-      <div className="absolute inset-0 flex items-end justify-center pb-32 px-4 sm:px-6 lg:px-8 mx-auto max-w-4xl md:max-w-2xl lg:max-w-4xl">
+      <div className="absolute inset-0 bg-blue-600/30 mix-blend-multiply z-0"></div>
+      <div className="absolute inset-0 z-10 flex items-end justify-center pb-32 px-4 sm:px-6 lg:px-8 mx-auto max-w-4xl md:max-w-2xl lg:max-w-4xl">
         <div className="text-center max-w-4xl">
           <h1 className="text-white text-3xl sm:text-2xl lg:text-4xl font-extrabold leading-tight drop-shadow-md">
             ¿Quieres que <span className="bg-primary-500 px-2 rounded text-secondary-700">+34,000 clientes</span> potenciales vean tu marca en nuestra pantalla LED de 11.1 m²?
